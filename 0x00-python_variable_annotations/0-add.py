faa72ae933e9a55a4dfe7947a988a5efbd4750d0
Jasemonu <jasemonu@gmail.com>
2023-10-05 21:11:41 +0000

    solution to task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/binenv python3
+"""A function that adds two floats"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers and return the result.
+
+    Args:
+        a (float): The first floating-point number.
+        b (float): The second floating-point number.
+
+    Returns:
+        float: The sum of the two numbers as a floating-point value.
+    """
+    return a + b
